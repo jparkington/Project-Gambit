22c5d89294e4870458c8a933084c5be30d41526a
James Parkington <pongs.acreage.0o@icloud.com>
2023-08-14 19:31:55 -0400

    Added more to-do items

---
+++ Objects/Application.py
@@ -0,0 +1,9 @@
+'''
+To-do:
+- Add a method to Dagger.py that finds the top three user_positions with centipawn velocity and collects their indeces
+- Update Dagger to use Parser class and existing Parquet directory for games using the Utility class
+- Update Navigator to use three game buttons
+- Add a centipawn plot to the bottom of Navigator's tkinter canvas
+- Update Navigator to use the results from Dagger (e.g. game 1's 5 moves, game 2's 5 moves, game 3's 5 moves)
+- Add a home button to navigator (with a clever icon--maybe a pawn) that takes you back to your game at the given index
+'''

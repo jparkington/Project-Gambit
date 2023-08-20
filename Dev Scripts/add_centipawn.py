9209b8ea8cc8fcad3e27fec87d7a1f2c0b048fd1
James Parkington <pongs.acreage.0o@icloud.com>
2023-08-20 18:03:02 -0400

    Schema fixes

--- Dev Scripts/add_centipawn.py
+++ Dev Scripts/add_centipawn.py
@@ -1,9 +1,8 @@
 from stockfish import Stockfish
 import chess.pgn
-import pandas as pd
+import pyarrow as pa
 import pyarrow.dataset as ds
 import pyarrow.parquet as pq
-import pyarrow as pa
 from io import StringIO
 
 def evaluate_position(pgn: str, stockfish_path: str, depth: int) -> int:
@@ -16,51 +15,36 @@ def evaluate_position(pgn: str, stockfish_path: str, depth: int) -> int:
     stockfish.set_fen_position(board.fen())
     return stockfish.get_evaluation()['value']
 
-def evaluate_chess_games(stockfish_path: str, depth: int, storage_directory: str):
-    # Create a dataset object
-    dataset = ds.dataset(storage_directory, format="parquet")
+# Directory containing the Parquet files
+parquet_dir = "/Users/Macington/Documents/Projects/Project Gambit/Games/Storage"
 
-    # Load the schema of the dataset
-    original_schema = dataset.schema
+# Read all Parquet files in the directory
+dataset = ds.dataset(parquet_dir, format="parquet")
+table = dataset.to_table()
 
-    # Create a new schema by casting all integer fields to float64
-    new_schema = pa.schema([(field.name, pa.float64() if pa.types.is_integer(field.type) else field.type) for field in original_schema])
+# Convert to Pandas DataFrame
+df = table.to_pandas()
 
-    # Load the entire dataset into a Table with the new schema
-    table = dataset.to_table(schema=new_schema)
+# Identify unique board_sum_in_context values where centipawn_evaluation is null
+unique_board_sums = df[df['centipawn_evaluation'].isnull()]['board_sum_in_context'].unique()
 
-    # Convert to pandas DataFrame
-    df = table.to_pandas()
+# Stockfish configuration
+stockfish_path = "Engines/Stockfish"
+depth = 10
 
-    # Filter records with null centipawn_evaluation
-    null_eval_df = df[df['centipawn_evaluation'].isnull()]
+# Evaluate each unique board_sum_in_context with Stockfish
+for board_sum_in_context in unique_board_sums:
+    # Get the corresponding pgn
+    pgn = df[df['board_sum_in_context'] == board_sum_in_context]['pgn'].iloc[0]
+    # Evaluate the position
+    centipawn_evaluation = evaluate_position(pgn, stockfish_path, depth)
+    # Update the DataFrame
+    df.loc[df['board_sum_in_context'] == board_sum_in_context, 'centipawn_evaluation'] = centipawn_evaluation
 
-    # Group by board_sum_in_context and sort by the count of null records
-    unique_board_sums = null_eval_df.groupby('board_sum_in_context').size().reset_index(name='counts').sort_values('counts', ascending=False)
+# Convert the updated DataFrame back to a pyarrow Table
+table = pa.Table.from_pandas(df)
 
-    # Evaluate each unique board_sum_in_context with Stockfish
-    for board_sum_in_context in unique_board_sums['board_sum_in_context']:
-        # Get the corresponding pgn
-        pgn = null_eval_df[null_eval_df['board_sum_in_context'] == board_sum_in_context]['pgn'].iloc[0]
-        # Evaluate the position
-        centipawn_evaluation = evaluate_position(pgn, stockfish_path, depth)
-        # Update the DataFrame
-        df.loc[df['board_sum_in_context'] == board_sum_in_context, 'centipawn_evaluation'] = centipawn_evaluation
+# Write the updated Table back to the Parquet directory
+pq.write_to_dataset(table, root_path=parquet_dir, partition_cols=dataset.partition_names)
 
-    # Convert back to pyarrow Table
-    updated_table = pa.Table.from_pandas(df)
-
-    # Write the updated Table back to the original location
-    pq.write_to_dataset(updated_table, root_path=storage_directory, format="parquet")
-
-    print("Processing completed!")m
-
-def main():
-    stockfish_path = "Engines/Stockfish"
-    depth = 10 # Adjust as needed
-    storage_directory = "/Users/Macington/Documents/Projects/Project Gambit/Games/Storage"
-
-    evaluate_chess_games(stockfish_path, depth, storage_directory)
-
-if __name__ == "__main__":
-    main()
+print("Missing centipawn_evaluation values have been filled based on board_sum_in_context.")

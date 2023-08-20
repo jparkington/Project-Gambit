efe79570e578993e4134278607898764e0f8b252
James Parkington <pongs.acreage.0o@icloud.com>
2023-08-20 17:26:05 -0400

    Removed `final_centipawn_value`

--- Dev Scripts/add_centipawn.py
+++ Dev Scripts/add_centipawn.py
@@ -1,93 +1,66 @@
-from concurrent.futures import ProcessPoolExecutor
 from stockfish import Stockfish
 import chess.pgn
 import pandas as pd
-import os
-from pathlib import Path
-from io import StringIO
-import gc
 import pyarrow.dataset as ds
 import pyarrow.parquet as pq
 import pyarrow as pa
+from io import StringIO
 
-def evaluate_game(args):
-    game_id, pgn, stockfish_path, depth = args
+def evaluate_position(pgn: str, stockfish_path: str, depth: int) -> int:
     stockfish = Stockfish(path=stockfish_path, depth=depth)
-    return _evaluate_game(game_id, pgn, stockfish)
-
-def _evaluate_game(game_id: int, pgn: str, stockfish: Stockfish) -> list:
     pgn_io = StringIO(pgn)
     game = chess.pgn.read_game(pgn_io)
-    evaluations = []
     board = game.board()
-    previous_evaluation = None
-
     for move in game.mainline_moves():
         board.push(move)
-        stockfish.set_fen_position(board.fen())
-        centipawn_evaluation = stockfish.get_evaluation()['value']
-        centipawn_diff = abs(centipawn_evaluation - previous_evaluation) if previous_evaluation is not None else 0
-        evaluations.append((game_id, board.ply(), centipawn_evaluation, centipawn_diff))
-        previous_evaluation = centipawn_evaluation
+    stockfish.set_fen_position(board.fen())
+    return stockfish.get_evaluation()['value']
 
-    return evaluations
-
-def evaluate_chess_games(stockfish_path: str, depth: int, storage_directory: str, start_ply: int, end_ply: int):
+def evaluate_chess_games(stockfish_path: str, depth: int, storage_directory: str):
     # Create a dataset object
     dataset = ds.dataset(storage_directory, format="parquet")
-    
-    # Loop through fragments (partitions) in the dataset
-    for fragment in dataset.get_fragments():
-        # Extract total_ply value directly from the fragment path
-        total_ply = int(fragment.path.split('/')[-2].split('=')[-1])
 
-        # Check if the total_ply is within the specified range
-        if start_ply <= total_ply <= end_ply:
-            print(f"Processing fragment at {fragment.path}...")
-            
-            # Read a fragment into a Table
-            table = fragment.to_table()
-            # Convert to pandas DataFrame
-            df = table.to_pandas()
-            
-            # Check if there are any NaN values in centipawn_evaluation
-            if df['centipawn_evaluation'].isnull().sum() == 0:
-                print("No NaN values found. Skipping...")
-                continue
-            
-            evaluations_to_update = {}
-            # Filter only the records with NaN centipawn_evaluation
-            df_null_eval = df[df['centipawn_evaluation'].isnull()]
-            with ProcessPoolExecutor() as executor:
-                tasks = [executor.submit(evaluate_game, (game_id, pgn, stockfish_path, depth)) for game_id, pgn in df_null_eval[['game_id', 'pgn']].itertuples(index=False)]
-                for task in tasks:
-                    for game_id, ply, centipawn_evaluation, _ in task.result():
-                        evaluations_to_update[df.loc[(df['game_id'] == game_id) & (df['ply'] == ply), 'board_sum_in_context'].iloc[0]] = centipawn_evaluation
-            
-            # Update centipawn_evaluation based on board_sum_in_context
-            for board_sum_in_context, centipawn_evaluation in evaluations_to_update.items():
-                df.loc[df['board_sum_in_context'] == board_sum_in_context, 'centipawn_evaluation'] = centipawn_evaluation
-            
-            # Convert back to pyarrow Table
-            updated_table = pa.Table.from_pandas(df)
-            
-            # Write the updated Table back to the fragment's location
-            pq.write_table(updated_table, fragment.path)
+    # Load the schema of the dataset
+    original_schema = dataset.schema
 
-            # Explicitly trigger garbage collection
-            gc.collect()
+    # Create a new schema by casting all integer fields to float64
+    new_schema = pa.schema([(field.name, pa.float64() if pa.types.is_integer(field.type) else field.type) for field in original_schema])
 
-    print("Processing completed!")
+    # Load the entire dataset into a Table with the new schema
+    table = dataset.to_table(schema=new_schema)
+
+    # Convert to pandas DataFrame
+    df = table.to_pandas()
+
+    # Filter records with null centipawn_evaluation
+    null_eval_df = df[df['centipawn_evaluation'].isnull()]
+
+    # Group by board_sum_in_context and sort by the count of null records
+    unique_board_sums = null_eval_df.groupby('board_sum_in_context').size().reset_index(name='counts').sort_values('counts', ascending=False)
+
+    # Evaluate each unique board_sum_in_context with Stockfish
+    for board_sum_in_context in unique_board_sums['board_sum_in_context']:
+        # Get the corresponding pgn
+        pgn = null_eval_df[null_eval_df['board_sum_in_context'] == board_sum_in_context]['pgn'].iloc[0]
+        # Evaluate the position
+        centipawn_evaluation = evaluate_position(pgn, stockfish_path, depth)
+        # Update the DataFrame
+        df.loc[df['board_sum_in_context'] == board_sum_in_context, 'centipawn_evaluation'] = centipawn_evaluation
+
+    # Convert back to pyarrow Table
+    updated_table = pa.Table.from_pandas(df)
+
+    # Write the updated Table back to the original location
+    pq.write_to_dataset(updated_table, root_path=storage_directory, format="parquet")
+
+    print("Processing completed!")m
 
 def main():
     stockfish_path = "Engines/Stockfish"
-    depth = 10 # Reduced depth
+    depth = 10 # Adjust as needed
     storage_directory = "/Users/Macington/Documents/Projects/Project Gambit/Games/Storage"
-    start_ply = 47
-    end_ply = 228
 
-    evaluate_chess_games(stockfish_path, depth, storage_directory, start_ply, end_ply)
+    evaluate_chess_games(stockfish_path, depth, storage_directory)
 
 if __name__ == "__main__":
     main()
-
